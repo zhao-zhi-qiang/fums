f5f43b403b736497bafb24e1bc694176a8b3f0d0
94975 <@wangshulei4415>
2020-07-02 11:23:26 +0800

    Initial Commit

---
+++ src/main/java/com/fums/FumsApplication.java
@@ -0,0 +1,13 @@
+package com.fums;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FumsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FumsApplication.class, args);
+    }
+
+}
