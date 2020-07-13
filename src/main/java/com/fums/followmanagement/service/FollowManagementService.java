132ca8b474605505a863f1258beaa1ecd98086ef
DBHZZQ <2672101702@qq.com>
2020-07-13 17:40:53 +0800

    2020年07月13日第一次上传

--- src/main/java/com/fums/followmanagement/service/FollowManagementService.java
+++ src/main/java/com/fums/followmanagement/service/FollowManagementService.java
@@ -1,8 +1,7 @@
 package com.fums.followmanagement.service;
 
 import com.fums.followmanagement.dao.FollowUpMapper;
-import com.fums.followmanagement.pojo.FollowUp;
-import com.fums.followmanagement.pojo.HospitalDepartmentChild;
+import com.fums.followmanagement.pojo.*;
 import org.apache.ibatis.annotations.Param;
 import org.springframework.beans.factory.annotation.Autowired;
 
@@ -42,12 +41,83 @@ public interface FollowManagementService {
      * @return
      */
     List<HospitalDepartmentChild> findDepartmentAll();
+
+    /**
+     * 查询单个随访组所有信息
+     * @param patientNumber
+     * @param admissionNumber
+     * @param setOfDate1
+     * @param setOfDate2
+     * @param sex
+     * @param name
+     * @return
+     */
+    List<Patient> findPatientAndFollow(String patientNumber, String admissionNumber,Date setOfDate1,Date setOfDate2,Integer sex,String name);
+
+    /**
+     * 查询所有用户角色
+     * @param roleName
+     * @return
+     */
+    List<User> findUserInfoAll(String roleName);
+
+    /**
+     * 条件查询所有用户
+     * @param userAccount
+     * @param hospitalDepartmentChildName
+     * @return
+     */
+    List<User> findUserAll(String userAccount,String hospitalDepartmentChildName);
+
+    /**
+     * 条件查询所有疾病编码
+     * @param id1
+     * @param id2
+     * @param jbmc00
+     * @return
+     */
+    List<DiseaseCoding> findDiseaseCodingAll(Integer id1,Integer id2,String jbmc00);
+
+    /**
+     * 根据角色查询所有该角色用户
+     * @param roleName
+     * @return
+     */
+    List<User> findUserAllByRoleName(String roleName);
+
     /**
      * 添加随访组
      * @param followUp
      * @return
      */
     boolean addFollowUpInfo(FollowUp followUp);
+
+    /**
+     * 添加患者
+     * @param patient
+     * @return
+     */
+    boolean addPatientInfo(Patient patient);
+
+    /**
+     * 添加单个随访组信息
+     * @param patient
+     * @return
+     */
+    boolean addFollowPatient(Patient patient);
+    /**
+     * 批量添加患者
+     * @param patient
+     * @return
+     */
+    boolean addPatientInfos(List<Patient> patient);
+
+    /**
+     * 添加角色
+     * @param role
+     * @return
+     */
+    boolean addRoleInfo(Role role);
 
     /**
      * 删除随访组信息
@@ -62,4 +132,18 @@ public interface FollowManagementService {
      * @return
      */
     boolean editFollowUpInfo(FollowUp followUp);
+
+    /**
+     * 编辑单个随访组内信息
+     * @param patient
+     * @return
+     */
+    boolean editFollowPatient(FollowPatient patient);
+
+    /**
+     * 删除单个随访组内信息
+     * @param id
+     * @return
+     */
+    boolean removeFollowPatient(Integer id);
 }
--- src/main/java/com/fums/followmanagement/service/impl/FollowManagementServiceImpl.java
+++ src/main/java/com/fums/followmanagement/service/impl/FollowManagementServiceImpl.java
@@ -1,9 +1,7 @@
 package com.fums.followmanagement.service.impl;
 
-import com.fums.followmanagement.dao.FollowUpMapper;
-import com.fums.followmanagement.dao.HospitalDepartmentChildMapper;
-import com.fums.followmanagement.pojo.FollowUp;
-import com.fums.followmanagement.pojo.HospitalDepartmentChild;
+import com.fums.followmanagement.dao.*;
+import com.fums.followmanagement.pojo.*;
 import com.fums.followmanagement.service.FollowManagementService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -27,7 +25,32 @@ public class FollowManagementServiceImpl implements FollowManagementService{
     private FollowUpMapper followUpMapper;
 
     @Autowired
+    private PatientMapper patientMapper;
+
+    @Autowired
+    private FollowPatientMapper followPatientMapper;
+
+    @Autowired
+    private UserMapper userMapper ;
+
+    @Autowired
     private HospitalDepartmentChildMapper hospitalDepartmentChildMapper;
+
+    @Autowired
+    private RoleMapper roleMapper;
+
+    @Autowired
+    private PowerMapper powerMapper;
+
+    @Autowired
+    private UserRoleMapper userRoleMapper;
+
+    @Autowired
+    private RolePowerMapper rolePowerMapper;
+
+    @Autowired
+    private DiseaseCodingMapper diseaseCodingMapper;
+
     /**
      * 验证随访组表单属性
      * @param followUpName
@@ -64,18 +87,140 @@ public class FollowManagementServiceImpl implements FollowManagementService{
     }
 
     /**
+     * 查询单个随访组所有信息
+     * @param patientNumber
+     * @param admissionNumber
+     * @param setOfDate1
+     * @param setOfDate2
+     * @param sex
+     * @param name
+     * @return
+     */
+    @Override
+    public List<Patient> findPatientAndFollow(String patientNumber, String admissionNumber, Date setOfDate1, Date setOfDate2, Integer sex, String name) {
+        return patientMapper.selectPatientAndFollow(patientNumber,admissionNumber,setOfDate1,setOfDate2,sex,name);
+    }
+
+    /**
+     * 查询所有用户角色信息
+     * @param roleName
+     * @return
+     */
+    @Override
+    public List<User> findUserInfoAll(String roleName) {
+        return userMapper.selectUserInfoAll(roleName);
+    }
+
+    /**
+     * 条件查询所有用户
+     * @param userAccount
+     * @param hospitalDepartmentChildName
+     * @return
+     */
+    @Override
+    public List<User> findUserAll(String userAccount, String hospitalDepartmentChildName) {
+        return userMapper.selectUserAll(userAccount,hospitalDepartmentChildName);
+    }
+
+    /**
+     * 条件查询所有疾病编码
+     * @param id1
+     * @param id2
+     * @param jbmc00
+     * @return
+     */
+    @Override
+    public List<DiseaseCoding> findDiseaseCodingAll(Integer id1, Integer id2, String jbmc00) {
+        return diseaseCodingMapper.selectDiseaseCodingAll(id1,id2,jbmc00);
+    }
+
+    /**
+     * 通过角色查询所有该角色用户
+     * @param roleName
+     * @return
+     */
+    @Override
+    public List<User> findUserAllByRoleName(String roleName) {
+        return userMapper.selectUserAllByRoleName(roleName);
+    }
+
+    /**
      * 添加随访组信息
      * @param followUp
      * @return
      */
     @Override
     public boolean addFollowUpInfo(FollowUp followUp) {
-        int i= followUpMapper.insert(followUp);
+        int i= followUpMapper.insertSelective(followUp);
+        FollowUp followUp1 = new FollowUp();
         if (i > 1){
             return true;
         }else {
             return false;
         }
+    }
+
+    /**
+     * 添加患者
+     * @param patient
+     * @return
+     */
+    @Override
+    public boolean addPatientInfo(Patient patient) {
+        int i = patientMapper.insert(patient);
+        if (i > 1){
+            return  true;
+        }else{
+            return false;
+        }
+    }
+
+    /**
+     * 添加单个随访组信息
+     * @param patient
+     * @return
+     */
+    @Override
+    public boolean addFollowPatient(Patient patient) {
+        FollowPatient followPatient = new FollowPatient();
+        followPatient.setPatientId(patient.getPatientId());
+        followPatient.setSetOfDate(patient.getFollowPatient().getSetOfDate());
+        int i = followPatientMapper.insert(followPatient);
+        patientMapper.insertSelective(patient);
+        if (i > 1){
+            return  true;
+        }else{
+            return false;
+        }
+    }
+
+    /**
+     * 批量添加患者
+     * @param patient
+     * @return
+     */
+    @Override
+    public boolean addPatientInfos(List<Patient> patient) {
+        int i = patientMapper.inserts(patient);
+        if (i > 1){
+            return  true;
+        }else{
+            return false;
+        }
+    }
+
+    /**
+     * 添加单个随访组信息
+     * @param role
+     * @return
+     */
+    @Override
+    public boolean addRoleInfo(Role role) {
+        int i = roleMapper.insert(role);
+        UserRole userRole = new UserRole();
+        userRole.setRoleId(role.getRoleId());
+        userRoleMapper.insert(userRole);
+        return false;
     }
 
     /**
@@ -104,6 +249,36 @@ public class FollowManagementServiceImpl implements FollowManagementService{
         if (i > 1){
             return true;
         }else {
+            return false;
+        }
+    }
+
+    /**
+     * 编辑单个随访组信息
+     * @param patient
+     * @return
+     */
+    @Override
+    public boolean editFollowPatient(FollowPatient patient) {
+        int i  = followPatientMapper.updateByPrimaryKeySelective(patient);
+        if (i > 0){
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    /**
+     * 删除单个随访组信息
+     * @param id
+     * @return
+     */
+    @Override
+    public boolean removeFollowPatient(Integer id) {
+        int i = followPatientMapper.deleteByPrimaryKey(id);
+        if (i > 0){
+            return true;
+        }else{
             return false;
         }
     }
